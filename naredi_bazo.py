e1c9d174285ddfe11b83d7e950247a8bafb38944
Jakob Gal <jg.frankovic@gmail.com>
2022-12-26 22:40:26 +0100

    Syntax error pri napolni podatke za zivilo

--- naredi_bazo.py
+++ naredi_bazo.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 db = sqlite3.connect("osebni_trener.db")
 
-df = pd.read_csv(r'data/nutrition.csv')
+df = pd.read_csv(r'data/modified.csv')
 met = []
 for col in df.columns:
     met.append(col) #met vsebuje imena stolpcev, dolzina je 77
@@ -148,8 +148,7 @@ def pripravi_bazo():
                 {met[72]} TEXT NOT NULL,
                 {met[73]} TEXT NOT NULL,
                 {met[74]} TEXT NOT NULL,
-                {met[75]} TEXT NOT NULL,
-                {met[76]} TEXT NOT NULL
+                {met[75]} TEXT NOT NULL
             );""")
      # Minerali (#8)
         cursor.execute("""CREATE TABLE IF NOT EXISTS Minerali
@@ -214,10 +213,10 @@ def pripravi_bazo():
 print(df)
 def napolni_nujne_podatke(conn):
     for row in df.itertuples():
-        with conn:
+        with conn: # mogoce je ena več ali ena manj
             conn.execute(f"""
-            INSERT INTO Zivilo ({met[1]}, {met[2]}, {met[3]}, {met[4]}, {met[5]}, {met[6]}, {met[7]}, {met[8]}, {met[9]}, {met[10]} , {met[11]} , {met[12]} ,{ met[13]} , {met[14]} , {met[15]} , {met[16]} , {met[17]} , {met[18]} , {met[19]} , {met[20]} , {met[21]} , {met[22]} , {met[23]} , {met[24]} , {met[25]} , {met[26]} , {met[27]} , {met[28]} ,{ met[29]} , {met[30]} , {met[31]} , {met[32]} , {met[33]} , {met[34]} , {met[35]} , {met[36]} , {met[37]} , {met[38]} , {met[39]} , {met[40]} , {met[41]} , {met[42]} , {met[43]} , {met[44]} , {met[45]} , {met[46]} , {met[47]} , {met[48]} , {met[49]} , {met[50]} , {met[51]} , {met[52]} ,{ met[53]} , {met[54]} , {met[55]} ,{ met[56]} , {met[57]} , {met[58]} ,{ met[59]} , {met[60]} , {met[61]} , {met[62]} , {met[63]} , {met[64]} , {met[65]} ,{met[66]} ,{ met[67]} , {met[68]} , {met[69]} , {met[70]} , {met[71]} , {met[72]} , {met[73]} , {met[74]} , {met[75]} , {met[76]} )
-            VALUES ('{row[2]}', '{row[3]}', '{row[4]}', '{row[5]}', '{row[6]}', '{row[7]}', '{row[8]}', '{row[9]}', '{row[10]}' , '{row[11]}' , '{row[12]}' , '{row[13]}' , '{row[14]}' , '{row[15]}' , '{row[16]}' , '{row[17]}' , '{row[18]}' , '{row[19]}' , '{row[20]}' , '{row[21]}' , '{row[22]}' , '{row[23]}' , '{row[24]}' , '{row[25]}' , '{row[26]}' , '{row[27]}' , '{row[28]}' , '{row[29]}' , '{row[30]}' , '{row[31]}' , '{row[32]}' , '{row[33]}' , '{row[34]}' , '{row[35]}' , '{row[36]}', '{row[37]}', '{row[38]}', '{row[39]}', '{row[40]}', '{row[41]}', '{row[42]}', '{row[43]}', '{row[44]}', '{row[45]}' , '{row[46]}' , '{row[47]}' , '{row[48]}' , '{row[49]}' , '{row[50]}' , '{row[51]}' , '{row[52]}' , '{row[53]}' , '{row[54]}' , '{row[55]}' , '{row[56]}' , '{row[57]}' , '{row[58]}' , '{row[59]}' , '{row[60]}' , '{row[61]}' , '{row[62]}' , '{row[63]}' , '{row[64]}' , '{row[65]}' , '{row[66]}' , '{row[67]}' , '{row[68]}' , '{row[69]}' , '{row[70]}', '{row[71]}', '{row[72]}', '{row[73]}', '{row[74]}', '{row[75]}', '{row[76]}', '{row[77]}')
+            INSERT INTO Zivilo ({met[0]}, {met[1]}, {met[2]}, {met[3]}, {met[4]}, {met[5]}, {met[6]}, {met[7]}, {met[8]}, {met[9]}, {met[10]} , {met[11]} , {met[12]} ,{ met[13]} , {met[14]} , {met[15]} , {met[16]} , {met[17]} , {met[18]} , {met[19]} , {met[20]} , {met[21]} , {met[22]} , {met[23]} , {met[24]} , {met[25]} , {met[26]} , {met[27]} , {met[28]} ,{ met[29]} , {met[30]} , {met[31]} , {met[32]} , {met[33]} , {met[34]} , {met[35]} , {met[36]} , {met[37]} , {met[38]} , {met[39]} , {met[40]} , {met[41]} , {met[42]} , {met[43]} , {met[44]} , {met[45]} , {met[46]} , {met[47]} , {met[48]} , {met[49]} , {met[50]} , {met[51]} , {met[52]} ,{ met[53]} , {met[54]} , {met[55]} ,{ met[56]} , {met[57]} , {met[58]} ,{ met[59]} , {met[60]} , {met[61]} , {met[62]} , {met[63]} , {met[64]} , {met[65]} ,{met[66]} ,{ met[67]} , {met[68]} , {met[69]} , {met[70]} , {met[71]} , {met[72]} , {met[73]} , {met[74]} )
+            VALUES ('{row[1]}', '{row[2]}', '{row[3]}', '{row[4]}', '{row[5]}', '{row[6]}', '{row[7]}', '{row[8]}', '{row[9]}', '{row[10]}' , '{row[11]}' , '{row[12]}' , '{row[13]}' , '{row[14]}' , '{row[15]}' , '{row[16]}' , '{row[17]}' , '{row[18]}' , '{row[19]}' , '{row[20]}' , '{row[21]}' , '{row[22]}' , '{row[23]}' , '{row[24]}' , '{row[25]}' , '{row[26]}' , '{row[27]}' , '{row[28]}' , '{row[29]}' , '{row[30]}' , '{row[31]}' , '{row[32]}' , '{row[33]}' , '{row[34]}' , '{row[35]}' , '{row[36]}', '{row[37]}', '{row[38]}', '{row[39]}', '{row[40]}', '{row[41]}', '{row[42]}', '{row[43]}', '{row[44]}', '{row[45]}' , '{row[46]}' , '{row[47]}' , '{row[48]}' , '{row[49]}' , '{row[50]}' , '{row[51]}' , '{row[52]}' , '{row[53]}' , '{row[54]}' , '{row[55]}' , '{row[56]}' , '{row[57]}' , '{row[58]}' , '{row[59]}' , '{row[60]}' , '{row[61]}' , '{row[62]}' , '{row[63]}' , '{row[64]}' , '{row[65]}' , '{row[66]}' , '{row[67]}' , '{row[68]}' , '{row[69]}' , '{row[70]}', '{row[71]}', '{row[72]}', '{row[73]}', '{row[74]}', '{row[75]}')
                         """)
 
 def pripravi_vse(conn):
